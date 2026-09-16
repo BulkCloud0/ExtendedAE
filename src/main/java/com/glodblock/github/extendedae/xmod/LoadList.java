b0a2805a218020224e359d9695dc552dedadf94d
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-16 15:45:16 -0300

    port: backport ExtendedAE mod detection to Java 8

--- src/main/java/com/glodblock/github/extendedae/xmod/LoadList.java
+++ src/main/java/com/glodblock/github/extendedae/xmod/LoadList.java
@@ -12,18 +12,14 @@ public final class LoadList {
     public static boolean REI = false;
     public static boolean GT = false;
 
-    public static Set<String> MOD_NAME = ModList.get().getMods().stream().flatMap(x -> Stream.of(x.getModId(), x.getDisplayName())).collect(Collectors.toSet());
+    public static Set<String> MOD_NAME = ModList.get().getMods().stream()
+            .flatMap(x -> Stream.of(x.getModId(), x.getDisplayName()))
+            .collect(Collectors.toSet());
 
     public static void init() {
-        var list = ModList.get();
-        if (list.isLoaded("jei")) {
-            JEI = true;
-        }
-        if (list.isLoaded("roughlyenoughitems")) {
-            REI = true;
-        }
-        if (list.isLoaded("gtceu")) {
-            GT = true;
-        }
+        ModList list = ModList.get();
+        JEI = list.isLoaded("jei");
+        REI = list.isLoaded("roughlyenoughitems");
+        GT = list.isLoaded("gtceu");
     }
 }
